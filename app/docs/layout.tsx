cc1e54c973f10405272e1b22e809b8a2037efa42
ju4n97 <ju4n97@proton.me>
2025-03-22 19:47:52 -0500

    Enhance layout configuration and add documentation structure for API, CLI, GUI, and Plugins

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,52 +1,45 @@
 import { baseOptions } from '@/app/layout.config';
 import { source } from '@/lib/source';
-import { DocsLayout } from 'fumadocs-ui/layouts/docs';
-import { BookOpen, Plug, Satellite, Terminal, Wallpaper } from 'lucide-react';
+import { DocsLayout, type DocsLayoutProps } from 'fumadocs-ui/layouts/docs';
 import type { ReactNode } from 'react';
 
+const docsLayoutOptions: DocsLayoutProps = {
+	...baseOptions,
+	tree: source.pageTree,
+	sidebar: {
+		tabs: {
+			transform(option, node) {
+				const meta = source.getNodeMeta(node);
+				if (!meta) {
+					return option;
+				}
+
+				const color = `var(--${meta.file.dirname}-color, var(--color-fd-foreground))`;
+
+				return {
+					...option,
+					icon: (
+						<div
+							className="rounded-md p-1 ring-2 [&_svg]:size-5"
+							style={
+								{
+									color,
+									border: `1px solid color-mix(in oklab, ${color} 50%, transparent)`,
+									'--tw-ring-color': `color-mix(in oklab, ${color} 20%, transparent)`,
+								} as object
+							}
+						>
+							{node.icon}
+						</div>
+					),
+				};
+			},
+		},
+		collapsible: false,
+	},
+	githubUrl: 'https://github.com/poqk/poqk',
+};
+
 export default function Layout({ children }: { children: ReactNode }) {
-	return (
-		<DocsLayout
-			{...baseOptions}
-			tree={source.pageTree}
-			sidebar={{
-				tabs: [
-					{
-						title: 'Guide',
-						description: 'Guide to start using Poqk',
-						icon: <BookOpen className="size-5 opacity-50" />,
-						url: '/docs/guide',
-					},
-					{
-						title: 'API',
-						description: 'API reference for integration',
-						icon: <Satellite className="size-5 opacity-50" />,
-						url: '/docs/api',
-					},
-					{
-						title: 'CLI',
-						description: 'Commands and usage details',
-						icon: <Terminal className="size-5 opacity-50" />,
-						url: '/docs/cli',
-					},
-					{
-						title: 'GUI',
-						description: 'Overview of graphical interface',
-						icon: <Wallpaper className="size-5 opacity-50" />,
-						url: '/docs/gui',
-					},
-					{
-						title: 'Plugins',
-						description: 'Extend Poqk with custom plugins',
-						icon: <Plug className="size-5 opacity-50" />,
-						url: '/docs/plugins',
-					},
-				],
-				collapsible: false,
-			}}
-			githubUrl="https://github.com/poqk/poqk"
-		>
-			{children}
-		</DocsLayout>
-	);
+	return <DocsLayout {...docsLayoutOptions}>{children}</DocsLayout>;
 }
